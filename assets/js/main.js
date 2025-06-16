fc05589c8da11ff442972516140b80f1ec8742cd
Matheus Kauan Dums <matheus.dums@buddemeyer.local>
2025-06-16 14:11:29 -0300

    Adicionando os arquivos ao GitHub

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/* let botao_altera = document.querySelector(".contact_btn");
+let container_form = document.querySelector("#form_container");
+
+function showForm() {
+  let classes_form = container_form.classList;
+  if (classes_form.contains("hide")) {
+    classes_form.classList.remove("hide");
+    classes_form.classList.add("enable");
+    botao_altera.innerHTML = "Fechar";
+  } else {
+    classes_form.classList.remove("enable");
+    classes_form.classList.add("hide");
+    botao_altera.innerHTML = "Adicionar Contato";
+  }
+} */
